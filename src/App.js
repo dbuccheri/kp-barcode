97e6a94340a9f186b9e1b74ba4a8263bd40da899
Daniele Buccheri <daniele.buccheri@gmail.com>
2023-01-23 14:52:36 -0800

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import { uidData } from "./uidData";
+ 
+ 
+ 
+ 
+const Component = () => {
+    const searchParams = new URLSearchParams(document.location.search)
+    const par_uid = searchParams.get('UID')
+    const getUID = uidData.filter (ID => ID.UID === par_uid )
+ 
+    return (
+        <div>
+        <div id="header">
+        <img src="./public/kplogo.png" width="200px" />
+        </div>
+        <h1 id="UID">
+            Unique Identifier Database
+        </h1>
+       <div > {getUID.map(data => (
+       <div>
+       <div id="data" class="parent">
+ 
+       
+        <div class="child">
+             <span class="label">UID:</span> {data.UID}<br />
+             <span class="label">Project:</span>  {data.project}<br />
+             <span class="label">Letter ID:</span>  <a href="https://sp-cloud.kp.org/sites/mssa-dms/LT/Forms/AllItems.aspx?FolderCTID=0x012000E13CEA7FFE72EA4BBF3018ECCEA7D61B&View=%7BF5A23DBB%2DFB79%2D47AB%2D87D2%2DB015C162A29E%7D&id=%2Fsites%2Fmssa%2Ddms%2FLT%2FKPMC%2FIntake&viewid=f5a23dbb%2Dfb79%2D47ab%2D87d2%2Db015c162a29e" title="Open BR from INTAKE"> {data.letterID}</a><br />
+             <span class="label">OTN/SKU No:</span> <a href={`https:cars.kp.org?SKU=${data.letterID}`} title="Open letter in CARS"> {data.otn_sku}</a><br />
+             <span class="label">Region:</span>  {data.region}<br />
+             <span class="label">Language:</span>  {data.language}<br />
+             <span class="label">Release Date:</span>  {data.release_date}<br />
+             <span class="label">Composition Date:</span>  {data.composition_date}<br />
+             <span class="label">Revision:</span>  {data.revision}<br />
+             <span class="label">Media:</span>  {data.media}<br />
+             <span class="label">Printing Vendor:</span>  {data.printing_vendor}<br />
+             <span class="label">Test Suite:</span>  {data.test_suite}<br />
+             <span class="label">Owner Group:</span>  {data.ownergroup}<br />
+             <span class="label">Client:</span>      <a href={`mailto:${data.client}?subject=Letter UID${data.UID}&body=Info requested`} title={`mailto:${data.client}`}> {data.client} </a><br />
+             <span class="label">TC Contact:</span>   <a href={`mailto:${data.tc}?subject=Letter UID${data.UID}&body=Info requested`} title={`mailto:${data.tc}`}>{data.tc} </a><br />
+             <span class="label">Notes:</span>   {data.notes}<br />
+        </div>
+       </div>
+       </div>
+      
+      ))}</div>
+        </div>
+    )
 }
 
-export default App;
+export default Component;
---
+++ src/uidData.js
@@ -0,0 +1,57 @@
+export const uidData = [
+  {
+    UID: "001",
+    project: "KPMC",
+    letterID: "MACL5502",
+    otn_sku: "KP42342",
+    region: "NCR",
+    language: "EN-ES-ZH",
+    release_date: "7-29-2022",
+    composition_date: "6-6-2022",   
+    revision: "1.2",
+    media: "Paper",
+    printing_vendor: "Arvato",
+    test_suite: "test001",
+    ownergroup: "DMSS",
+    client: "kpmc.client@kp.org",
+    tc: "daniele.x.buccheri@kp.org",
+    notes:"Lorem Ipsum is simply dummya adasd a dasd a dasd asd assd  adsds as asd sa"
+      },
+  {
+    UID: "002",
+    project: "METRS",
+    letterID: "0.28",
+    otn_sku: "MT45234",
+    region: "SCR",
+    language: "EN-ES-ZH",
+    release_date: "1-2-2022",
+    composition_date: "2-1-2022",  
+    revision: "3.0",
+    media: "Digital/Paper",
+    printing_vendor: "Arvato",
+    test_suite: "Test003",
+    ownergroup: "DMSS",
+    client: "metrs.client@kp.org",
+    tc: "daniele.x.buccheri@kp.org",
+    notes:"Lorem Ipsum has been the industry's standard dummy"
+  },
+  {
+    UID: "003",
+    project: "PSP",
+    letterID: "PSP3242",
+    otn_sku: "P76534",
+    region: "NCR",
+    language: "EN-ES-ZH",
+    release_date: "3-9-2022",
+    composition_date: "11-26-2021",  
+    revision: "2.0",
+    media: "Digital",
+    printing_vendor: "N/A",
+    test_suite: "Test005",
+    ownergroup: "DMSS",
+    client: "psp.client@kp.org",
+    tc: "daniele.x.buccheri@kp.org",
+    notes:"Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  
+  }
+];
